d420c2b56202d5a3a22c9cf42d54c62c28666e3a
Ziifauzii11 <57211455+Anshalsa@users.noreply.github.com>
2021-03-29 14:05:33 +0700

    18090087

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Tugas Laravel</title>
+</head>
+
+<body>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.0.45/css/materialdesignicons.min.css" integrity="sha256-nRyVCcVDSlWN9d68r9M+rKBLE4k9Cp1j3XSY/umjvvU=" crossorigin="anonymous" />
+    <div class="container">
+        <div class="row mb-3 mt-3">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header bg-white">
+                            <h5 class="card-title text-black mb-0">Data Diri</h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="xp-social-profile">
+                                <div class="xp-social-profile-middle">
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <div class="xp-social-profile-title">
+                                                <p class="my-1 text-black">Nama : Asri Nihal Salsabila</p>
+                                            </div>
+                                            <div class="xp-social-profile-title">
+                                                <p class="my-1 text-black">NIM : 18090087</p>
+                                            </div>
+                                            <div class="xp-social-profile-title">
+                                                <p class="my-1 text-black">Kelas : 6B</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            <!-- Optional JavaScript; choose one of the two! -->
+
+            <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
